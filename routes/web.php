88c2cf5b5f1eca6335feea885aaa9756685c9993
Kresna Satya <kresnasatya72@gmail.com>
2024-11-24 10:44:45 +0800

    fix: route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::redirect('/', 'home');
 Volt::route('/', 'home')->name('home');
 Volt::route('/search', 'search')->name('search');
 Volt::route('/users', 'users')->name('users');
